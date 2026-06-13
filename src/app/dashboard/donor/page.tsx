9fbb88a42b6de92a0c5912adf423aaf6440a4a9a
Navitha <navithakudumula@gmail.com>
2026-06-13 08:46:06 +0530

    Update page.tsx

--- src/app/dashboard/donor/page.tsx
+++ src/app/dashboard/donor/page.tsx
@@ -1,5 +1,5 @@
+"use client";
 import { cn } from "@/lib/utils";
-"use client"
 
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
